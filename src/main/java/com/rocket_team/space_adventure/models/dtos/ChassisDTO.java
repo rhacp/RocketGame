f30183f6ceb4f7a8f6ad150e2c2771497086dcc1
Dan <inb3zna@gmail.com>
2023-12-22 16:10:14 +0200

    starting basic structure

---
+++ src/main/java/com/rocket_team/space_adventure/models/dtos/ChassisDTO.java
@@ -0,0 +1,12 @@
+package com.rocket_team.space_adventure.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class ChassisDTO {
+
+    private Long id;
+    private int speed;
+    private int shield;
+    private int fuel;
+}
